f69dcbacf8a22f13666564824ef38db6d2d8419a
taiyuuki <taiyuuki@qq.com>
2023-05-10 23:46:34 +0800

    fix: 修正bug

--- README.md
+++ README.md
@@ -1,3 +1,56 @@
-### vscode-nes
+# 红白机模拟器
 
-NES emulator for vscode
+这是一款可以玩 FC(NES) 游戏的 VS Code 插件。
+
+受 [小霸王](https://marketplace.visualstudio.com/items?itemName=gamedilong.anes) 的启发，但它已经很久没有维护，不太能用了，于是我写了这个能用、且功能更强大的版本。
+
+## 功能
+
+* 支持手柄
+* 支持连发键
+* 支持即时保存、读取
+* 支持切换分辨率
+* 支持暂停、静音
+* 支持加载本地ROM
+* 支持双人
+
+## 使用
+
+安装插件后，点击侧边栏的图标，然后选择游戏就可以玩了。
+
+由于安全策略的问题，每次选择游戏都需要手动点击屏幕中间的 `点我开始游戏` 才会运行。
+
+如果黑屏、不显示文字，点重置按钮试试，或者重新点击侧边栏中的游戏。
+
+## 配置项
+
+我默认设置了20多个游戏，用的是Github Page作为远程地址，不太需要担心会失效，但理论上依旧有失效的可能性。
+
+根据网速的不同情况，个别游戏可能需要加载较长时间。
+
+如果想玩更多游戏，除了加载本地ROM，你还可以在插件的设置（`settings.json`）中增加或修改`vscodeNes.romPath`，格式如下：
+
+```json
+"vscodeNes.romPath": [
+    [
+      "忍者龙剑传3-黄泉方舟",
+      "https://taiyuuki.github.io/vscode-nes/roms/忍者龙剑传3-黄泉方舟.nes"
+    ],
+    [
+      "快打旋风",
+      "https://taiyuuki.github.io/vscode-nes/roms/快打旋风.nes"
+    ],
+    [
+      "恶魔城",
+      "https://taiyuuki.github.io/vscode-nes/roms/恶魔城.nes"
+    ]
+  ]
+```
+
+## Issue
+
+如果遇到任何问题或bug，欢迎提交[issue](https://github.com/taiyuuki/vscode-nes/issues)。
+
+## License
+
+MIT Copyright (c) 2023 Taiyuuki
--- res/webview/main.js
+++ res/webview/main.js
@@ -6,6 +6,8 @@ ctx.height = '240'
 let fpsInterval = 0
 let romBuffer = null
 
+let req = new XMLHttpRequest()
+
 function inBrowser() {
   return typeof acquireVsCodeApi === 'undefined'
 }
@@ -174,15 +176,15 @@ function loadROM(url) {
     start()
     return
   }
-  const req = new XMLHttpRequest()
+  req = new XMLHttpRequest()
   req.open('GET', url)
   req.overrideMimeType('text/plain; charset=x-user-defined')
-  req.timeout = 10000
+  req.timeout = 300000
   req.ontimeout = () => {
-    emitError(`${title.textContent}请求超时，地址可能已经失效。`)
+    emitError(`${title.textContent}请求超时，可能是地址失效或网络不稳定。`)
   }
   req.onerror = () => {
-    emitError(`${title.textContent}加载失败，地址可能已经失效。`)
+    emitError(`${title.textContent}加载失败，可能是地址失效或网络不稳定。`)
   }
   req.onload = function () {
     if (this.status === 200) {
@@ -198,6 +200,56 @@ function loadROM(url) {
   req.send()
 }
 
+// 保存游戏
+const saveBtn = document.getElementById('save')
+let saveTimeout = null
+saveBtn.addEventListener('click', () => {
+  if (nes.cpu.irqRequested && !isStop && !isPause) {
+    const data = JSON.stringify({ data: nes.toJSON(), name: title.textContent })
+    localStorage.setItem('nes', data)
+    saveBtn.value = '已存'
+    if (saveTimeout) {
+      clearTimeout(saveTimeout)
+    }
+    saveTimeout = setTimeout(() => {
+      saveBtn.value = '保存'
+      saveTimeout = null
+    }, 1000)
+  }
+})
+
+// 读取游戏
+const loadBtn = document.getElementById('load')
+let loadTimeout = null
+loadBtn.addEventListener('click', () => {
+  if (nes.cpu.irqRequested && !isStop && !isPause) {
+    const saveData = JSON.parse(localStorage.getItem('nes'))
+    if (saveData) {
+      if (saveData.name !== title.textContent) {
+        return emitError(`当前游戏[${title.textContent}]与保存的数据[${saveData.name}]不一致`)
+      }
+      try {
+        nes.ppu.reset()
+        nes.romData = saveData.data.romData
+        nes.cpu.fromJSON(saveData.data.cpu)
+        nes.mmap.fromJSON(saveData.data.mmap)
+        nes.ppu.fromJSON(saveData.data.ppu)
+        loadBtn.value = '已读'
+        if (loadTimeout) {
+          clearTimeout(loadTimeout)
+        }
+        loadTimeout = setTimeout(() => {
+          loadBtn.value = '读取'
+          loadTimeout = null
+        }, 1000)
+      }
+      catch (_) {
+        return emitError('无法读取，请开始游戏后重试')
+      }
+    }
+  }
+})
+
 window.addEventListener('message', (e) => {
   showEl(mask)// 显示遮罩
   hiddenEl(startBtn)// 隐藏开始按钮
@@ -206,6 +258,7 @@ window.addEventListener('message', (e) => {
     title.textContent = e.data.lable
     romBuffer = null
   }
+  req.abort()
   loadROM(e.data.url)
 })
 
